e9dbfd0df90efe566ef0ea0e5acd92dacff432a8
majeed13 <majeem@uw.edu>
2020-06-01 14:14:59 -0700

    Added openCV Canny detector function using cpu (I belive no gpu usage in the standard Canny call)

--- kernel.cu
+++ kernel.cu
@@ -3,12 +3,19 @@
 #include "device_launch_parameters.h"
 #include <stdio.h>
 #include "opencv2/highgui/highgui.hpp"
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 using namespace std;
 using namespace cv;
 
+void cpu_canny();
+
 int main()
 {
+    cpu_canny();
+    
+    // Test to see if we can read BGR values
+    /*
     Mat img;
     img = imread("Engine_in.PNG");
     int rows = img.rows;
@@ -29,5 +36,51 @@ int main()
             cout << endl;
         }
     }
+    */
     return 0;
+}
+
+void cpu_canny()
+{
+    // openCV Matrices to read images
+    Mat src, src_gray;
+    Mat dst, detected_edges;
+
+    // Canny variables
+    int edgeThresh;
+    int lowThresh = 100;
+    int const max_lowThresh = 100;
+    int ratio = 3;
+    int kernel_size = 3;
+
+    // Window Name
+    char* window_name = "Edge Map";
+    // Read image
+    src = imread("Engine_in.PNG");
+    if (!src.data)
+    {
+        cout << "No src picture found" << endl;
+    }
+    // Create matrix of same size and type
+    dst.create(src.size(), src.type());
+    // Covnert to grayscale
+    cvtColor(src, src_gray, COLOR_BGR2GRAY);
+    // Reduce noise (can experiment with size)
+    blur(src_gray, detected_edges, Size(3, 3));
+    // openCV Canny detecor
+    Canny(detected_edges, detected_edges, lowThresh, lowThresh * ratio, kernel_size);
+    
+    // Explained as: "USING CANNY'S OUTPUT AS A MASK, DISPLAY RESULTS"
+    //this fills dst with all 0s making it completely black
+    dst = Scalar::all(0);
+
+    // Copy src image to dst. copies only the pixelsin the locations where
+    // they have non-zero vals. Since output of teh Canny detector is the edge
+    // contours on a black background, the resulting dst will be black in all
+    // the area but the detected edges
+    src.copyTo(dst, detected_edges);
+    // Display result
+    imshow(window_name, dst);
+    // Wait for key press.
+    waitKey(0);
 }
